558d8e4b576f51881a061f73f2d1c526be6ded96
garibatt <12537@holbertonstudents.com>
2025-11-22 21:08:29 +0000

    salam

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
     roman = {
         "I": 1,
         "V": 5,
@@ -11,13 +14,12 @@ def roman_to_int(roman_string):
     }
 
     total = 0
-    if type(roman_string) is not str:
-        return 0
     for index, char in enumerate(roman_string):
         if index == len(roman_string) - 1:
-            total += roman[char]
+            total += roman.get(char, 0)
         elif roman[char] < roman[roman_string[index + 1]]:
             total -= roman[char]
         else:
             total += roman[char]
+
     return total
