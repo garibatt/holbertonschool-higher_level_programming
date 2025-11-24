a6a913ebb1b7b6e5da13eba06edf90a4e74c3048
garibatt <12537@holbertonstudents.com>
2025-11-24 20:40:50 +0000

    salam

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This is documentation of the class."""
+"""This is documentation of the Rectangle class."""
 
 
 class Rectangle:
-    """This is a Square class."""
+    """This is a Rectangle class."""
 
     def __init__(self, width=0, height=0):
-        """Initialize the square with optional size."""
+        """Initialize the rectangle with optional width and height."""
         self.width = width
         self.height = height
 
@@ -19,31 +19,31 @@ class Rectangle:
     def width(self, value):
         """Set width with validation."""
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Retrieve height"""
+        """Retrieve height."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
-        """Set height with validation"""
+        """Set height with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
-        """Return arena"""
-        return self.__width * self.__height 
-    
+        """Return area."""
+        return self.__width * self.__height
+
     def perimeter(self):
-        """Return perimetr"""
+        """Return perimeter."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
