3aa25c7fd5fbf6e912f19bef1262799d92836d2a
garibatt <12537@holbertonstudents.com>
2025-11-26 15:29:10 +0000

    salam

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+document
+"""
+class MyList(list):
+    """
+    this line creates list
+    """
+    def print_sorted(self):
+        """
+        this line print list
+        """
+        print(sorted(self))
