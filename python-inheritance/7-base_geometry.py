a5bad249f4e9d28e7fc12c336536927c7044aacb
garibatt <12537@holbertonstudents.com>
2025-11-27 14:58:54 +0000

    salam

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""This is the BaseGeometry class module."""
+"""This is the BaseGeometry class module"""
 
 
 class BaseGeometry:
-    """BaseGeometry class."""
+    """BaseGeometry class"""
 
     def area(self):
-        """Raise an exception because area is not implemented."""
         raise Exception("area() is not implemented")
     """this is document"""
 
