3927c959531938e16fd5a466d5677d823f9dbd64
garibatt <12537@holbertonstudents.com>
2025-11-27 14:30:56 +0000

    salam

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this is document"""
+
+
+class BaseGeometry:
+    """this is also document"""
+    pass
+    
+    def area(self):
+        raise Exception(area() is not implemented)
