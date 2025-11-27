2379d30f57749df2208c7c50fbca1a145c15461e
garibatt <12537@holbertonstudents.com>
2025-11-27 14:00:53 +0000

    salam

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""this is document"""
+"""this is object"""
 
 
 def is_kind_of_class(obj, a_class):
-    """this is function"""
-    return isinstance(obj, a_class:)
+    """this is documnet"""
+    return isinstance(obj, a_class)
