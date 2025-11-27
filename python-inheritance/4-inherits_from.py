3c9aa7bb9192d315d3d74d773c93980b97493325
garibatt <12537@holbertonstudents.com>
2025-11-27 14:09:28 +0000

    salam

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-"""this line creates something"""
+    """this line creates something"""
     a = isinstance(obj, a_class)
     b = type(obj) is not a_class
     return a and b
