ed8d3ab0ecfc86e41285e216216e5cbc9692d957
garibatt <12537@holbertonstudents.com>
2025-11-28 14:06:51 +0000

    salam

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is document"""
+
+
+def read_file(filename=""):
+    """this is document for read file"""
+    with open(filename, "w",  encoding="utf-8") as f:
+        return f.write(text)
