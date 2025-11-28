3eb51eebe76389f17e7fb07728acceb054feed2e
garibatt <12537@holbertonstudents.com>
2025-11-28 13:52:03 +0000

    salam

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this is document"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """this is document"""
+    def __init__(self, size):
+        """this is document"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
