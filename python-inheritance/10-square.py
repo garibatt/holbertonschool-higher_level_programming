aeeeaab3722f67a1d204062dad6b99acd968da2a
garibatt <12537@holbertonstudents.com>
2025-11-28 13:51:25 +0000

    salam

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """this is document"""
-
-
 Rectangle = __import__("9-rectangle").Rectangle
+
 
 class Square(Rectangle):
     """this is document"""
-    def __init__(size,size):
-        """this is document""" 
-         self.integer_validator("size", size)
-         super().__init__(size, size)
-         self.__size = size
+    def __init__(self, size):
+        """this is document"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
