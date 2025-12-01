8b34e23574cff9f338091306659740acdf5f1170
garibatt <12537@holbertonstudents.com>
2025-12-01 16:14:58 +0000

    salam

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this is comment line"""
+
+from urllib import request
+
+url = "https://intranet.hbtn.io/status"
+
+with request.urlopen(url) as response:
+    body = response.read()
+
+    print("Body response:")
+    print("    - type:", type(body))
+    print("    - content:", body)
+    print("    - utf8 content:", body.decode("utf-8"))
+
