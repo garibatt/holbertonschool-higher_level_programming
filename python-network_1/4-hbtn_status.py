2835d7db6b3eae9e923a9140af915c043afb0dfc
garibatt <12537@holbertonstudents.com>
2025-12-02 16:15:01 +0000

    salam

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""simple request (1line)"""
+import requests
+
+if __name__ == "__main__":
+    req = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t- content: {req.text}")
