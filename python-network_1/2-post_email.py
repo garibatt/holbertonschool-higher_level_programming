1ce756fa85b490c229bc0e34df6e25311cad7d4d
garibatt <12537@holbertonstudents.com>
2025-12-02 15:18:28 +0000

    salam

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-""" this is comment line"""
+"""POST request with urllib"""
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
-    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
-    req = urllib.request.Request(url, data=data, method="POST")
 
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode("utf-8"))
+    data = {"email": email}
+
+    endata = urllib.parse.urlencode(data).encode("utf-8")
+
+    req = urllib.request.Request(url, data=endata)
+
+    with urllib.request.urlopen(req) as f:
+        body = f.read().decode("utf-8")
+        print(body)
